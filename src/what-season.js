b040ba6bf19bc1716de0d8a1f2a4bf84c6efff4f
Mash2607 <142324880+Mash2607@users.noreply.github.com>
2023-08-15 17:33:36 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,49 @@
-const CustomError = require("../extensions/custom-error");
+const { NotImplementedError } = require('../extensions/index.js');
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/**
+ * Extract season from given date and expose the enemy scout!
+ * 
+ * @param {Date | FakeDate} date real or fake date
+ * @returns {String} time of the year
+ * 
+ * @example
+ * 
+ * getSeason(new Date(2020, 02, 31)) => 'spring'
+ * 
+ */
+function getSeason(date) {
+  if(date === undefined) return 'Unable to determine the time of year!';
+  
+  try {
+    date.getTime();
+  } catch (error) {
+    throw new Error ('Invalid date!');
+  }
+
+            switch(date.getMonth()) {
+                case 11:
+                case 0:
+                case 1:
+                return  'winter';
+                break;       
+                case 2:
+                case 3:
+                case 4:
+                return  'spring';
+                break;       
+                case 5:
+                case 6:
+                case 7:
+                return  'summer';
+                break;       
+                case 8:
+                case 9:
+                case 10:
+                return  'autumn';
+                break;       
+            }
+}
+
+module.exports = {
+  getSeason
 };
